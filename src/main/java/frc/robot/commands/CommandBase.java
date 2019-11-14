39d3729925c3ab980f8fd54d8042e86afa98c74a
robotics <robotics@LO0145W28076833.thompsonschools.org>
2019-11-14 15:07:47 -0700

    Added Drive Code

--- src/main/java/frc/robot/commands/CommandBase.java
+++ src/main/java/frc/robot/commands/CommandBase.java
@@ -2,14 +2,14 @@ package frc.robot.commands;
 
 import frc.robot.OI;
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.subsystems.MotorTest;
+import frc.robot.subsystems.Drive;
 
 public abstract class CommandBase extends Command {
 	/**
 	 * Inherited static access to the OI.
 	 */
 	protected static OI oi;
-	protected static MotorTest motorTest;
+	protected static Drive drive;
 	/**
 	 * Initialize the CommandBase, in doing so accessing and initializing every
 	 * primary subsystem on the robot. Use this during robot initialization to
@@ -18,6 +18,6 @@ public abstract class CommandBase extends Command {
 	
 	public static void init() {
 		oi = OI.getInstance();
-		motorTest = MotorTest.getInstance();
+		drive = Drive.getInstance();
 	}
 }
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,127 @@
+package frc.robot.subsystems;
+
+import frc.robot.RobotMap;
+import frc.robot.commands.drive.UserDrive;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+//import com.ctre.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drive extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	
+	private static Drive instance;
+	//used for getInstance
+	int directCoefficient = 1;
+	private boolean direction = true;
+	private boolean speedToggle = false;
+	//for speedToggle function to halve speed
+	
+	/*private TALON frontLeft;
+	private TALON frontRight;
+	private TALON backLeft;
+	private TALON backRight;*/
+	
+	
+	private TalonSRX frontLeft;
+	private TalonSRX frontRight;
+	private TalonSRX backLeft;
+	private TalonSRX backRight;
+	
+	
+	private UserDrive userDrive;
+	
+	//May use if this we need to
+	//robotDrive.setInvertedMotor(MotorType.frontLeft, true);
+	
+	private double voltageCoefficient = 1;
+	private double turnPowerCoefficient = 1;
+
+	
+	public Drive(){
+		//Define motor controllers for drive train on test chassis
+		frontLeft = new TalonSRX(RobotMap.DRIVE_FRONT_LEFT_TAlON);
+    	frontRight = new TalonSRX(RobotMap.DRIVE_FRONT_RIGHT_TALON);
+    	backLeft = new TalonSRX(RobotMap.DRIVE_BACK_LEFT_TALON);
+    	backRight = new TalonSRX(RobotMap.DRIVE_BACK_RIGHT_TALON);
+    	
+    	/* Define motor controllers for drive train on main chassis
+    	 * Need this because FIRST only has TALON plugins and they have no idea what its like trying to find and install plugins from third party sites that look fairly sketchy and don't really install right and why can't just add the commonly used motor controllers that they have in their FIRST Choice Program
+    	 * http://www.ctr-electronics.com/downloads/pdf/CTRE%20Toolsuite%20Installation%20Guide.pdf*/
+    /*	frontLeft = new TalonSRX(RobotMap.DRIVE_FRONT_LEFT_TALON);
+    	frontRight = new TalonSRX(RobotMap.DRIVE_FRONT_RIGHT_TALON);
+    	backLeft = new TalonSRX(RobotMap.DRIVE_BACK_LEFT_TALON);
+    	backRight = new TalonSRX(RobotMap.DRIVE_BACK_RIGHT_TALON);*/
+    	
+	}
+	
+	public void initDefaultCommand() {
+	       userDrive = new UserDrive();
+	       setDefaultCommand(userDrive);
+	    }
+	
+	public static Drive getInstance() {
+    	if (instance == null) {
+    		instance = new Drive();
+    	}
+    	return instance;
+    }
+	
+	/**
+     * Set the four drive TALONs based upon the specified power values 
+     * multiplied by the drivetrain voltage coefficient.
+     * @param hPower Horizontal (strafing) power.  Pulled from the X axis of the
+     * left stick. Positive goes right!
+     * @param vPower Vertical (forward) power.  Pulled from the Y axis of the 
+     * left stick. Positive goes forward!
+     * @param turn Turning power.  Pulled from the left and right triggers.
+     */
+	
+	ControlMode iJustWantToSleep = ControlMode.PercentOutput;
+	public void drive(double hPower, double vPower, double turn) {
+        frontLeft.set(iJustWantToSleep, (vPower - hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
+        frontRight.set(iJustWantToSleep, (vPower + hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
+        backLeft.set(iJustWantToSleep, (-vPower - hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
+        backRight.set(iJustWantToSleep, (-vPower + hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
+    }
+	
+	public void stop() {
+    	drive(0, 0, 0);
+    }
+	
+	public boolean isSpeedToggle() {
+		
+    	return speedToggle;
+    }
+    
+    public void setSpeedToggle(boolean speedToggle) {
+    	this.speedToggle = speedToggle;
+    	if (speedToggle) {
+    		voltageCoefficient = 0.5;
+    	} else {
+    		voltageCoefficient = 1.0;
+    	}
+	}
+	
+	public boolean isForward() {
+		
+    	return this.direction;
+    }
+    
+    public void setDirection(boolean direction) {
+    	this.direction = direction;
+    	if (direction) {
+    		directCoefficient = 1;
+    	} else {
+    		directCoefficient = -1;
+		}
+		System.out.println("Direction is " + directCoefficient);
+    }
+}
