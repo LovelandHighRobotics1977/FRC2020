925e7e1181e1d878871678fd4d6c29b24477c77d
robotics <robotics@LO0145W26076831.thompsonschools.org>
2020-01-14 15:51:05 -0700

    drive code

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -86,10 +86,10 @@ public class Drive extends Subsystem {
 	
 	ControlMode iJustWantToSleep = ControlMode.PercentOutput;
 	public void drive(double vPower, double turn) {
-        frontLeft.set(iJustWantToSleep, );
-        frontRight.set(iJustWantToSleep, (vPower + hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
-        backLeft.set(iJustWantToSleep, (-vPower - hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
-        backRight.set(iJustWantToSleep, (-vPower + hPower + (turn * turnPowerCoefficient)) * voltageCoefficient * directCoefficient);
+        frontLeft.set(iJustWantToSleep, vPower);
+        frontRight.set(iJustWantToSleep, vPower);
+        backLeft.set(iJustWantToSleep, vPower);
+        backRight.set(iJustWantToSleep, vPower);
     }
 	
 	public void stop() {
