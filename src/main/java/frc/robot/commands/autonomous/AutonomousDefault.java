1c26dd86eab18972e808dc17a46cbcc259285bbd
robotics <robotics@LO0145W29076834.thompsonschools.org>
2020-01-21 15:43:51 -0700

    added AutonomousDefault

--- src/main/java/frc/robot/commands/autonomous/AutonomousDefault.java
+++ src/main/java/frc/robot/commands/autonomous/AutonomousDefault.java
@@ -3,28 +3,21 @@ package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-//import frc.robot.commands.drive.*;
+import frc.robot.commands.drive.*;
 
 public class AutonomousDefault extends CommandGroup {	
 	
-	//int time;
+	int time;
 	public AutonomousDefault() {
 		// Pause for 118 or any additional teams
-		/*time = 0;
+		time = 0;
 		addTime(3000);
 		addSequential(new DriveTime(0, time));
-		addTime(3000);
-		addSequential(new DriveTime(.4, time));
-		//addSequential(new DriveSide(.4, .4, 13000));	
-		addTime(4000);
-		addSequential(new TurnTime(time, 0.5));
-		addTime(2000);
-		addSequential(new DriveTime(.4, time));
 	}
 	// This function allows us to only set the time we want the function to run for, rather than manually needing to add up
 	// the time eaach time
 	void addTime(int timeAdd) {
-		time += timeAdd;*/
+		time += timeAdd;
 	}
 	
 }
