4d7afbbf8e22923f7e81a48a6290f648bd01c60d
robotics <robotics@LO0145W29076834.thompsonschools.org>
2020-01-21 15:04:07 -0700

    Began specific team work

--- src/main/java/frc/robot/commands/CommandBase.java
+++ src/main/java/frc/robot/commands/CommandBase.java
@@ -3,6 +3,8 @@ package frc.robot.commands;
 import frc.robot.OI;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Pneumatics;
+
 
 public abstract class CommandBase extends Command {
 	/**
@@ -10,6 +12,7 @@ public abstract class CommandBase extends Command {
 	 */
 	protected static OI oi;
 	protected static Drive drive;
+	protected static Pneumatics pneumatics;
 	/**
 	 * Initialize the CommandBase, in doing so accessing and initializing every
 	 * primary subsystem on the robot. Use this during robot initialization to
@@ -19,5 +22,6 @@ public abstract class CommandBase extends Command {
 	public static void init() {
 		oi = OI.getInstance();
 		drive = Drive.getInstance();
+		pneumatics = Pneumatics.getInstance();
 	}
 }
---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,72 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.Compressor;
+//import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Pneumatics extends Subsystem{
+	public static Pneumatics instance;
+	private Compressor compressor1;
+	//Relay compressorRelay = new Relay(0);
+    DoubleSolenoid liftPiston;
+    DoubleSolenoid dumpPiston;
+
+	
+	
+	Pneumatics(){
+        liftPiston = new DoubleSolenoid(6, 7);
+        dumpPiston = new DoubleSolenoid(4,5);
+		compressor1 = new Compressor(0);
+		compressor1.setClosedLoopControl(true);
+		//compressorRelay.set(Relay.Value.kOn);
+		compressor1.start();
+		System.out.print("Compressing running? " + compressor1.enabled());
+	}
+	
+	
+	public static Pneumatics getInstance() {
+		if (instance == null) {
+    		instance = new Pneumatics();
+    	}
+    	return instance;
+	}
+	
+	public void climb() {
+		liftPiston.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	public void lower() {
+		liftPiston.set(DoubleSolenoid.Value.kReverse);
+    }
+    
+    public void dump(){
+        dumpPiston.set(DoubleSolenoid.Value.kForward);
+    }
+    public void back(){
+        dumpPiston.set(DoubleSolenoid.Value.kReverse);
+    }
+	
+	public void stop() {
+        liftPiston.set(DoubleSolenoid.Value.kOff);
+        dumpPiston.set(DoubleSolenoid.Value.kOff);
+		//compressorRelay.set(Relay.Value.kOff);
+	}
+	
+	/*public void startClosedLoop(){
+		compressor1.setClosedLoopControl(true);
+	}*/
+	
+	public void stopCompressor() {
+		compressor1.stop();
+		
+	}
+
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+}
