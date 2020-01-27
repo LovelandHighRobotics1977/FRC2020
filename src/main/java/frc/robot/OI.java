16891919857a199b2b258f72e919d88337bdcbc4
robotics <robotics@LO0145W26076831.thompsonschools.org>
2020-01-27 15:37:17 -0700

    Started controller mapping

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import frc.robot.input.XBoxController;
+import frc.robot.commands.pneumatics.*;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -66,7 +67,7 @@ public class OI {
 		//driveJoystick.rightWhenPressed(new TurnTime(500,1)); //THIS NEEDS TO BE Adjusted
 		//auxJoystick.startWhenPressed(new CommandAutonomous());
 		//auxJoystick.rightThumbWhileHeld(command);
-         
+         driveJoystick.rightThumbWhenPressed(new hookExtend());
 	}
 	
 	public static OI getInstance() {
