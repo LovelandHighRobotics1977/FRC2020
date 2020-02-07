a78b474633d8211cb46cd6ef4e11f6763bbd737c
robotics <robotics@LO0145W26076831.thompsonschools.org>
2020-02-07 15:27:35 -0700

    Removed MotorTest

--- src/main/java/frc/robot/commands/CommandBase.java
+++ src/main/java/frc/robot/commands/CommandBase.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 import frc.robot.subsystems.Drive;
 import frc.robot.OI;
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.subsystems.MotorTest;
 import frc.robot.subsystems.Pneumatic;
 import frc.robot.subsystems.ColorWheel;
 
@@ -11,7 +10,6 @@ public abstract class CommandBase extends Command {
 	 * Inherited static access to the OI.
 	 */
 	protected static OI oi;
-	protected static MotorTest motorTest;
 	protected static Drive drive;
 	protected static Pneumatic pneumatic;
 	protected static ColorWheel colorwheel;
@@ -23,7 +21,6 @@ public abstract class CommandBase extends Command {
 	
 	public static void init() {
 		oi = OI.getInstance();
-		motorTest = MotorTest.getInstance();
 		drive = Drive.getInstance();
 		colorwheel = ColorWheel.getInstance();
 	}
